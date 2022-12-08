1e476c47014a7c5223697624d492d77a1630428d
Ethan Shifflett <76451302+eshifflett@users.noreply.github.com>
2022-12-07 19:31:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const axios = require('axios');
 const app = express();
+const port = process.env.PORT || 3001;
 
 // view engine
 app.set("views", path.resolve(__dirname, "templates"));
@@ -142,3 +143,5 @@ async function getDog(){
     }
     return result;
 }
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
