245fc3ff3903703f089cbd69a7e327e9f7b43bbb
Ethan Shifflett <76451302+eshifflett@users.noreply.github.com>
2022-12-07 19:49:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.static(path.join(__dirname, 'scripts')));
 app.use(bodyParser.urlencoded({extended:false}));
 
 // env loading
-require("dotenv").config({ path: path.resolve(__dirname, '.env') })
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const db = process.env.MONGO_DB_NAME;
